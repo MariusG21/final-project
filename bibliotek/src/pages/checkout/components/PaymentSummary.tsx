94f79de9d2b88535c005fade8555f292e9f3c934
MariusG21 <gheoneamarius10@gmail.com>
2025-07-01 10:41:25 +0300

    add styles to PaymentSummary

--- bibliotek/src/pages/checkout/components/PaymentSummary.tsx
+++ bibliotek/src/pages/checkout/components/PaymentSummary.tsx
@@ -1,25 +1,27 @@
+import styles from "./PaymentSummary.module.css";
+
 export function PaymentSummary() {
   return (
-    <div className="payment-summary">
-      <div className="rows">
-        <div className="label">Items:</div>
-        <div className="price">0</div>
+    <div className={styles["payment-summary"]}>
+      <div className={styles["rows"]}>
+        <div className={styles["label"]}>Items:</div>
+        <div className={styles["price"]}>0</div>
       </div>
-      <div className="rows">
-        <div className="label">Subtotal:</div>
-        <div className="price">0</div>
+      <div className={styles["rows"]}>
+        <div className={styles["label"]}>Subtotal:</div>
+        <div className={styles["price"]}>0</div>
       </div>
-      <div className="rows">
-        <div className="label">Tax Rate:</div>
-        <div className="price">0</div>
+      <div className={styles["rows"]}>
+        <div className={styles["label"]}>Tax Rate:</div>
+        <div className={styles["price"]}>0</div>
       </div>
-      <div className="rows">
-        <div className="label">Tax:</div>
-        <div className="price">0</div>
+      <div className={styles["rows"]}>
+        <div className={styles["label"]}>Tax:</div>
+        <div className={styles["price"]}>0</div>
       </div>
-      <div className="rows">
-        <div className="label">Total:</div>
-        <div className="price">0</div>
+      <div className={styles["rows"]}>
+        <div className={styles["label"]}>Total:</div>
+        <div className={styles["price"]}>0</div>
       </div>
     </div>
   );
