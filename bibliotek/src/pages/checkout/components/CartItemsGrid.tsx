e83fc1086378a9e3a844e98ad6ee88bafda2802f
MariusG21 <gheoneamarius10@gmail.com>
2025-07-01 17:27:05 +0300

    add styles to CartItemsGrid

--- bibliotek/src/pages/checkout/components/CartItemsGrid.tsx
+++ bibliotek/src/pages/checkout/components/CartItemsGrid.tsx
@@ -30,6 +30,33 @@ export function CartItemsGrid() {
           </div>
         </div>
       </div>
+      <div className={styles["cart-item"]}>
+        <div className={styles["cover-image"]}>
+          <img src="/images/books/the-dark-forest.jpg" alt="#" className="" />
+        </div>
+        <div className={styles["book-details"]}>
+          <div className={styles["book-info"]}>
+            <div className={styles["book-title"]}>
+              <span className={styles["label"]}>Title</span>Lorem
+            </div>
+            <div className={styles["book-author"]}>
+              <span className={styles["label"]}>Author</span>lorem
+            </div>
+            <div className={styles["discount"]}>
+              <span className={styles["label"]}>Discount</span>10%
+            </div>
+            <div className={styles["price"]}>
+              <span className={styles["label"]}>Price</span>$10.99
+            </div>
+          </div>
+          <div className={styles["book-actions"]}>
+            <div className={styles["remove"]}>remove</div>
+            <div className={styles["bookmark-and-remove"]}>
+              bookmark & remove
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
