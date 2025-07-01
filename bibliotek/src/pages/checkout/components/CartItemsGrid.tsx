6c6b63adba94f9305eba1d49812d1d7a20c52475
MariusG21 <gheoneamarius10@gmail.com>
2025-07-01 21:47:18 +0300

    create responsive layout for CheckoutPage

--- bibliotek/src/pages/checkout/components/CartItemsGrid.tsx
+++ bibliotek/src/pages/checkout/components/CartItemsGrid.tsx
@@ -1,58 +1,44 @@
+import { Link } from "react-router";
 import styles from "./CartItemsGrid.module.css";
+import { SeparatorLine } from "@/components/SeparatorLine/SeparatorLine";
 
 export function CartItemsGrid() {
   return (
     <div className={styles["cart-items-container"]}>
       <div className={styles["cart-item"]}>
-        <div className={styles["cover-image"]}>
-          <img src="/images/books/the-dark-forest.jpg" alt="#" className="" />
-        </div>
+        <Link
+          to={`/books/${"id-here"}`}
+          className={styles["cover-image-container"]}
+        >
+          <img
+            src="/images/books/the-dark-forest.jpg"
+            alt="#"
+            className={styles["cover-image"]}
+          />
+        </Link>
         <div className={styles["book-details"]}>
           <div className={styles["book-info"]}>
             <div className={styles["book-title"]}>
-              <span className={styles["label"]}>Title</span>Lorem
+              <span className={styles["label"]}>Title:</span>The Count of Monte
+              Cristo
             </div>
             <div className={styles["book-author"]}>
-              <span className={styles["label"]}>Author</span>lorem
+              <span className={styles["label"]}>Author:</span>lorem
             </div>
             <div className={styles["discount"]}>
-              <span className={styles["label"]}>Discount</span>10%
+              <span className={styles["label"]}>Discount:</span>10%
             </div>
             <div className={styles["price"]}>
-              <span className={styles["label"]}>Price</span>$10.99
+              <span className={styles["label"]}>Price:</span>$10.99
             </div>
           </div>
+          <SeparatorLine />
           <div className={styles["book-actions"]}>
-            <div className={styles["remove"]}>remove</div>
-            <div className={styles["bookmark-and-remove"]}>
-              bookmark & remove
+            <div role="button" className={styles["remove-button"]}>
+              Remove
             </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles["cart-item"]}>
-        <div className={styles["cover-image"]}>
-          <img src="/images/books/the-dark-forest.jpg" alt="#" className="" />
-        </div>
-        <div className={styles["book-details"]}>
-          <div className={styles["book-info"]}>
-            <div className={styles["book-title"]}>
-              <span className={styles["label"]}>Title</span>Lorem
-            </div>
-            <div className={styles["book-author"]}>
-              <span className={styles["label"]}>Author</span>lorem
-            </div>
-            <div className={styles["discount"]}>
-              <span className={styles["label"]}>Discount</span>10%
-            </div>
-            <div className={styles["price"]}>
-              <span className={styles["label"]}>Price</span>$10.99
-            </div>
-          </div>
-          <div className={styles["book-actions"]}>
-            <div className={styles["remove"]}>remove</div>
-            <div className={styles["bookmark-and-remove"]}>
-              bookmark & remove
+            <div role="button" className={styles["bookmark-and-remove-button"]}>
+              Save for Later
             </div>
           </div>
         </div>
