8a2fa18dd8f13bb670eb381672e8465d67c030e4
MariusG21 <gheoneamarius10@gmail.com>
2025-07-02 10:48:45 +0300

    display login-message if no user

--- bibliotek/src/pages/checkout/CheckoutPage.tsx
+++ bibliotek/src/pages/checkout/CheckoutPage.tsx
@@ -4,16 +4,28 @@ import { PaymentSummary } from "./components/PaymentSummary";
 import { CartItemsGrid } from "./components/CartItemsGrid";
 import { CheckoutHeader } from "./components/CheckoutHeader";
 import styles from "./Checkout.module.css";
+import { useAuthContext } from "@/context/auth/useAuthContext";
+import { Link } from "react-router";
 
 export function CheckoutPage() {
+  const { user } = useAuthContext();
+
   return (
     <>
       <Header />
       <Sidebar />
       <div className={styles["checkout-page"]}>
         <CheckoutHeader />
-        <PaymentSummary />
-        <CartItemsGrid />
+        {!user ? (
+          <p>
+            Please login to access your cart. <Link to="/login">Login</Link>
+          </p>
+        ) : (
+          <>
+            <PaymentSummary />
+            <CartItemsGrid />
+          </>
+        )}
       </div>
     </>
   );
