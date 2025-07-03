a3edd1502b3ec4710434c068849b306a852f72f9
MariusG21 <gheoneamarius10@gmail.com>
2025-07-03 21:32:06 +0300

    delete the cart when user is destroyed

--- bibliotek-backend/models/associations.js
+++ bibliotek-backend/models/associations.js
@@ -2,8 +2,8 @@ import { User } from "./User.js";
 import { Book } from "./Book.js";
 import { Cart } from "./Cart.js";
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
+User.hasOne(Cart, { onDelete: "CASCADE" });
+Cart.belongsTo(User, { onDelete: "CASCADE" });
 
 Book.belongsToMany(Cart, { through: "CartBooks" });
 Cart.belongsToMany(Book, { through: "CartBooks" });
