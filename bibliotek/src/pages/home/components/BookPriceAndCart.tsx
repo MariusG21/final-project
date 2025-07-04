3223ffc2bbf41e8109c364a2b8de7df79ff8e54e
MariusG21 <gheoneamarius10@gmail.com>
2025-07-04 17:56:25 +0300

    implement adding to cart functionality

--- bibliotek/src/pages/home/components/BookPriceAndCart.tsx
+++ bibliotek/src/pages/home/components/BookPriceAndCart.tsx
@@ -1,16 +1,47 @@
+import axios from "axios";
+import { useAuthContext } from "@/context/auth/useAuthContext";
 import { BookPrice } from "@/components/Price/BookPrice";
 import type { BookPreview } from "@/types/Book";
 import styles from "./BookPriceAndCart.module.css";
+import toast from "react-hot-toast";
 
-type BookPriceAndCartProps = Pick<BookPreview, "price" | "discount">;
+type BookPriceAndCartProps = Pick<BookPreview, "price" | "discount" | "id">;
 
-export function BookPriceAndCart({ price, discount }: BookPriceAndCartProps) {
+export function BookPriceAndCart({
+  price,
+  discount,
+  id,
+}: BookPriceAndCartProps) {
+  const { user, accessToken } = useAuthContext();
+
+  const addToCart = async () => {
+    if (!user) {
+      toast.error("Please login before adding to cart");
+      return;
+    }
+    const { data } = await axios.post(
+      "/api/cart/items",
+      {
+        id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log(data.data);
+  };
+
   return (
     <>
       <BookPrice price={price} discount={discount} />
 
       <div className={`${styles["add-to-cart-border"]} primary-border`}>
-        <div className={`${styles["add-to-cart-button"]} primary-button`}>
+        <div
+          className={`${styles["add-to-cart-button"]} primary-button`}
+          onClick={addToCart}
+        >
           Add to Cart
         </div>
       </div>
