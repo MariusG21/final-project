0d2a2edc7085267952ad16a1e9c7a48ae487db02
MariusG21 <gheoneamarius10@gmail.com>
2025-07-11 21:00:26 +0300

    conditionally render logo-holder based on screen size to prevent flickering

--- bibliotek/src/components/Header/components/Logo.tsx
+++ bibliotek/src/components/Header/components/Logo.tsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router";
+import { useScreenSizeContext } from "@/context/screenSize/useScreenSizeContext";
 import logoHolder from "@/assets/images/logoHolder.svg";
 import logoHolderSmall from "@/assets/images/logo-holder-small.png";
 import logo from "@/assets/images/logo.png";
 import styles from "./Logo.module.css";
 
 export function Logo() {
+  const { isSmall } = useScreenSizeContext();
+
   return (
     <div className={styles["logo-container"]}>
-      <picture>
-        <source media="(max-width: 768px)" srcSet={logoHolderSmall} />
-        <img src={logoHolder} className={styles["logo-holder"]} />
-      </picture>
+      <img
+        src={isSmall ? logoHolderSmall : logoHolder}
+        className={
+          isSmall ? styles["logo-holder-small"] : styles["logo-holder"]
+        }
+      />
       <Link to="/" className={styles["wrapper"]}>
         <img className={styles["logo"]} src={logo} alt="" />
-        <span className={styles["logo-text"]}>Bibliotek</span>
+        {!isSmall && <span className={styles["logo-text"]}>Bibliotek</span>}
       </Link>
     </div>
   );
