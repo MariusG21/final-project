5d87471fe0801e6fe46247ba48e7064a3754dc55
MariusG21 <gheoneamarius10@gmail.com>
2025-07-23 18:39:27 +0300

    create associations user-bookshelf and bookshelf-books

--- bibliotek-backend/models/associations.js
+++ bibliotek-backend/models/associations.js
@@ -1,9 +1,16 @@
 import { User } from "./User.js";
 import { Book } from "./Book.js";
 import { Cart } from "./Cart.js";
+import { Bookshelf } from "./Bookshelf.js";
 
 User.hasOne(Cart, { onDelete: "CASCADE" });
 Cart.belongsTo(User, { onDelete: "CASCADE" });
 
 Book.belongsToMany(Cart, { through: "CartBooks" });
 Cart.belongsToMany(Book, { through: "CartBooks" });
+
+User.hasOne(Bookshelf, { onDelete: "CASCADE" });
+Bookshelf.belongsTo(User, { onDelete: "CASCADE" });
+
+Book.belongsToMany(Bookshelf, { through: "BookshelfBooks" });
+Bookshelf.belongsToMany(Book, { through: "BookshelfBooks" });
--- bibliotek-backend/models/index.js
+++ bibliotek-backend/models/index.js
@@ -3,7 +3,8 @@ import { sequelize } from "./sequelize.js";
 import { User } from "./User.js";
 import { Book } from "./Book.js";
 import { Cart } from "./Cart.js";
+import { Bookshelf } from "./Bookshelf.js";
 
 import "./associations.js";
 
-export { sequelize, User, Book, Cart };
+export { sequelize, User, Book, Cart, Bookshelf };
