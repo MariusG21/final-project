7f162eb26e29bc5245d30eb02de4b853749b128a
MariusG21 <gheoneamarius10@gmail.com>
2025-07-24 20:54:26 +0300

    replace addToCart function with the hook version

--- bibliotek/src/pages/home/components/BookPriceAndCart.tsx
+++ bibliotek/src/pages/home/components/BookPriceAndCart.tsx
@@ -1,9 +1,5 @@
-import axios from "axios";
-import { toast } from "react-toastify";
-import { useAuthContext } from "@/context/auth/useAuthContext";
-import { useCartBooksContext } from "@/context/cartBooks/useCartBooksContext";
-import { useCartTotalsContext } from "@/context/cartTotals/useCartTotalsContext";
 import { BookPrice } from "@/components/Price/BookPrice";
+import { useAddToCart } from "@/hooks/useAddToCart";
 import type { BookPreview } from "@/types/Book";
 import styles from "./BookPriceAndCart.module.css";
 
@@ -14,53 +10,7 @@ export function BookPriceAndCart({
   discount,
   id,
 }: BookPriceAndCartProps) {
-  const { user, accessToken } = useAuthContext();
-  const { fetchCartBooks } = useCartBooksContext();
-  const { fetchCartTotals } = useCartTotalsContext();
-
-  const addToCart = async () => {
-    if (!user) {
-      toast.error("Please login before adding to cart");
-      return;
-    }
-    try {
-      const { data } = await axios.post(
-        "/api/cart/items",
-        {
-          id,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      if (data.success) {
-        toast.success(data.message);
-        fetchCartBooks();
-        fetchCartTotals();
-      }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        if (error.response) {
-          const { status } = error.response;
-          const message =
-            error.response.data?.message || "Unexpected server error.";
-
-          if (status === 409) {
-            toast.info(message);
-          } else if (status === 404 || status === 401) {
-            toast.error(message);
-          } else {
-            toast.error(message);
-          }
-        }
-      } else {
-        console.error(error);
-        toast.error("Something went wrong");
-      }
-    }
-  };
+  const { addToCart } = useAddToCart();
 
   return (
     <>
@@ -69,7 +19,7 @@ export function BookPriceAndCart({
       <div className={`${styles["add-to-cart-border"]} primary-border`}>
         <div
           className={`${styles["add-to-cart-button"]} primary-button`}
-          onClick={addToCart}
+          onClick={() => addToCart(id)}
         >
           Add to Cart
         </div>
