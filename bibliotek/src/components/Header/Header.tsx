df6d89d7cbbb1b5d8fa6a7cb42e0e2c6f686ca40
MariusG21 <gheoneamarius10@gmail.com>
2025-07-29 20:20:52 +0300

    delete checkout-link local component and replace it with CartButton

--- bibliotek/src/components/Header/Header.tsx
+++ bibliotek/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { useScreenSizeContext } from "@/context/screenSize/useScreenSizeContext";
 import { SeparatorLine } from "@/components/SeparatorLine/SeparatorLine";
+import { CartButton } from "@/components/Buttons/CartButton";
 import { TopLinks } from "./components/TopLinks";
 import { Logo } from "./components/Logo";
 import { SearchBar } from "./components/SearchBar";
-import { CheckoutLink } from "./components/CheckoutLink";
 import { UserProfile } from "./components/UserProfile";
 import { HamburgerMenu } from "./components/HamburgerMenu";
 import styles from "./Header.module.css";
@@ -19,7 +19,12 @@ export function Header() {
         <SearchBar />
         <div className={styles["right-section"]}>
           {isSmall && <HamburgerMenu />}
-          <CheckoutLink />
+          <CartButton
+            action="cartLink"
+            label="CART"
+            width={isSmall ? 4.6 : 10.2}
+            widthUnits="rem"
+          />
           <SeparatorLine direction="v" color="third" />
           <UserProfile />
         </div>
