10ed4d3c0efd03480a6318ee5cf7545c16e3d457
MariusG21 <gheoneamarius10@gmail.com>
2025-07-29 09:21:13 +0300

    create badge

--- bibliotek/src/components/Sidebar/components/SidebarLink.tsx
+++ bibliotek/src/components/Sidebar/components/SidebarLink.tsx
@@ -1,20 +1,28 @@
 import { NavLink } from "react-router";
 import type { LucideIcon } from "lucide-react";
 import styles from "./SidebarLink.module.css";
+import type { ReactNode } from "react";
 
 type SidebarLinkProps = {
   label: string;
   icon: LucideIcon;
   to: string;
+  children?: ReactNode;
 };
 
-export function SidebarLink({ to, label, icon: Icon }: SidebarLinkProps) {
+export function SidebarLink({
+  to,
+  label,
+  icon: Icon,
+  children,
+}: SidebarLinkProps) {
   return (
     <NavLink to={to} className={styles["links"]}>
       <div className={styles["links-icons"]}>
         <Icon />
       </div>
       {label}
+      {children}
     </NavLink>
   );
 }
--- bibliotek/src/components/Sidebar/components/SidebarNav.tsx
+++ bibliotek/src/components/Sidebar/components/SidebarNav.tsx
@@ -1,12 +1,21 @@
 import { SidebarLink } from "./SidebarLink";
 import { BookOpen, Bookmark, Clock, TrendingUp } from "lucide-react";
 import styles from "./SidebarNav.module.css";
+import { useFavoriteBooksContext } from "@/context/favoriteBooks/useFavoriteBooksContext";
+import { useBookshelfContext } from "@/context/bookshelf/useBookshelfContext";
 
 export function SidebarNav() {
+  const { favoriteCount } = useFavoriteBooksContext();
+  const { booksCount } = useBookshelfContext();
+
   return (
     <nav className={styles["book-menu"]}>
-      <SidebarLink to="/bookshelf" label="Bookshelf" icon={BookOpen} />
-      <SidebarLink to="/bookmarked" label="Bookmarked" icon={Bookmark} />
+      <SidebarLink to="/bookshelf" label="Bookshelf" icon={BookOpen}>
+        {booksCount !== 0 && <span>{booksCount}</span>}
+      </SidebarLink>
+      <SidebarLink to="/bookmarked" label="Bookmarked" icon={Bookmark}>
+        {favoriteCount !== 0 && <span>{favoriteCount}</span>}
+      </SidebarLink>
       <SidebarLink to="/best-sellers" label="Best Sellers" icon={TrendingUp} />
       <SidebarLink
         to="/continue-reading"
