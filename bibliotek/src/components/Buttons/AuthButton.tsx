22fa6be10722d84f6cde33ac7003f6347da33d9a
MariusG21 <gheoneamarius10@gmail.com>
2025-08-01 11:49:07 +0300

    create Auth button

---
+++ bibliotek/src/components/Buttons/AuthButton.tsx
@@ -0,0 +1,53 @@
+import { useRedirect } from "@/hooks/redirect/useRedirect";
+import styles from "./AuthButton.module.css";
+
+type AuthButtonProps = {
+  width: number;
+  height: number;
+  borderRadius: number;
+  label: "Login" | "Register" | "Back";
+  type: "button" | "submit";
+  action: "link" | "submit" | "goBack";
+};
+
+export function AuthButton({
+  width = 12,
+  height = 4.6,
+  borderRadius = 10,
+  label = "Login",
+  type = "button",
+  action = "link",
+}: Partial<AuthButtonProps>) {
+  const { redirectTo, redirectBackOr } = useRedirect();
+
+  const handleEvent = () => {
+    switch (action) {
+      case "link":
+        redirectTo("/login");
+        break;
+      case "goBack":
+        redirectBackOr();
+        break;
+      case "submit":
+        console.log("Form submitted");
+        break;
+    }
+  };
+
+  const style = {
+    width: `${width}rem`,
+    height: `${height}rem`,
+    borderRadius: `${borderRadius}rem`,
+  };
+
+  return (
+    <button
+      onClick={handleEvent}
+      type={type}
+      className={styles["auth-button"]}
+      style={style}
+    >
+      {label}
+    </button>
+  );
+}
