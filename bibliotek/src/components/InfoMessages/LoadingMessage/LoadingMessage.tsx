bff8089fc38e590135d4ec22d59fb8114decb2b2
MariusG21 <gheoneamarius10@gmail.com>
2025-08-04 19:42:25 +0300

    add spinning animation for LoadingMessage component

--- bibliotek/src/components/InfoMessages/LoadingMessage/LoadingMessage.tsx
+++ bibliotek/src/components/InfoMessages/LoadingMessage/LoadingMessage.tsx
@@ -1,3 +1,7 @@
+import Lottie from "lottie-react";
+import loadingSpinner from "@/assets/animations/loading-spinner.json";
+import styles from "./LoadingMessage.module.css";
+
 type LoadingMessageProps = {
   message: string;
   position?: string;
@@ -7,5 +11,19 @@ export function LoadingMessage({
   message = "Loading...",
   position,
 }: LoadingMessageProps) {
-  return <div className={`info-message ${position}`}>{message}</div>;
+  return (
+    <div className={`info-message ${position}`}>
+      <div className={styles["wrapper"]}>
+        {message}
+        <Lottie
+          animationData={loadingSpinner}
+          loop
+          style={{
+            width: 130,
+            margin: "5px -40px 0 -40px",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
