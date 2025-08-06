abb4bb44d07283189a137b06aa8c6a15b81c27f9
MariusG21 <gheoneamarius10@gmail.com>
2025-08-06 12:25:03 +0300

    display message if no books

--- bibliotek/src/pages/home/HomePage.tsx
+++ bibliotek/src/pages/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/components/Header/Header";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { ErrorMessage } from "@/components/InfoMessages/ErrorMessage/ErrorMessage";
 import { LoadingMessage } from "@/components/InfoMessages/LoadingMessage/LoadingMessage";
+import { InfoMessage } from "@/components/InfoMessages/InfoMessage/InfoMessage";
 import { useBooks } from "@/hooks/books/useBooks";
 import { BooksGrid } from "./components/BooksGrid";
 import styles from "./HomePage.module.css";
@@ -19,6 +20,8 @@ export function HomePage() {
           <LoadingMessage message="Loading books..." />
         ) : error ? (
           <ErrorMessage message={error} />
+        ) : books.length === 0 ? (
+          <InfoMessage message="No books available" />
         ) : (
           <BooksGrid books={books} />
         )}
