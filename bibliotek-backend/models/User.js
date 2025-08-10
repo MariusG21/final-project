86d271f5aa2c8d84217f8545adc9e647e59645cd
MariusG21 <gheoneamarius10@gmail.com>
2025-08-10 10:53:42 +0300

    redefine User model

--- bibliotek-backend/models/User.js
+++ bibliotek-backend/models/User.js
@@ -1,29 +1,66 @@
 import { sequelize } from "./sequelize.js";
 import { DataTypes } from "sequelize";
 
-export const User = sequelize.define("user", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    set(value) {
-      this.setDataValue("username", value.trim());
+export const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      set(value) {
+        this.setDataValue("username", value.trim());
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        this.setDataValue("email", value.trim().toLowerCase());
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      defaultValue: "",
+    },
+    favoriteBook: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "",
+    },
+    favoriteAuthor: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "",
+    },
+    favoriteGenre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "",
     },
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    set(value) {
-      this.setDataValue("email", value.trim().toLowerCase());
+  {
+    defaultScope: {
+      attributes: ["username", "id"],
     },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+
+    scopes: {
+      withPassword: {
+        attributes: ["username", "id", "password"],
+      },
+      withDetails: {
+        attributes: { exclude: ["password", "updatedAt"] },
+      },
+    },
+  }
+);
