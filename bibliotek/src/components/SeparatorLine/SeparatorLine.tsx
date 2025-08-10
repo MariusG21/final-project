1e344e0a1ab167fae30b84644029a14f8ffed385
MariusG21 <gheoneamarius10@gmail.com>
2025-08-10 10:48:59 +0300

    improve separator line to support margin

--- bibliotek/src/components/SeparatorLine/SeparatorLine.tsx
+++ bibliotek/src/components/SeparatorLine/SeparatorLine.tsx
@@ -6,11 +6,31 @@ type Color = "primary" | "secondary" | "third";
 type SeparatorLineProps = {
   direction?: Direction;
   color?: Color;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
 };
 
 export function SeparatorLine({
   direction = "h",
   color = "primary",
+  marginTop = 0,
+  marginBottom = 0,
+  marginLeft = 0,
+  marginRight = 0,
 }: SeparatorLineProps) {
-  return <div className={`${styles[direction]} ${styles[color]}`}></div>;
+  const style = {
+    marginTop: `${marginTop}rem`,
+    marginBottom: `${marginBottom}rem`,
+    marginLeft: `${marginLeft}rem`,
+    marginRight: `${marginRight}rem`,
+  };
+
+  return (
+    <div
+      className={`${styles[direction]} ${styles[color]}`}
+      style={style}
+    ></div>
+  );
 }
