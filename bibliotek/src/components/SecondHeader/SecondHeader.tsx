8f6ee785bb4c6891778a965ee357eaf6e3b3aef3
MariusG21 <gheoneamarius10@gmail.com>
2025-08-15 22:39:13 +0300

    add optional back button in SecondHeader component

--- bibliotek/src/components/SecondHeader/SecondHeader.tsx
+++ bibliotek/src/components/SecondHeader/SecondHeader.tsx
@@ -1,9 +1,27 @@
+import { FaArrowLeft } from "react-icons/fa";
+import { useRedirect } from "@/hooks/redirect/useRedirect";
 import styles from "./SecondHeader.module.css";
 
 type SecondHeaderType = {
   title: string;
+  withBackButton?: boolean;
 };
 
-export function SecondHeader({ title }: SecondHeaderType) {
-  return <header className={styles["header"]}>{title}</header>;
+export function SecondHeader({ title, withBackButton }: SecondHeaderType) {
+  const { redirectBackOr } = useRedirect();
+
+  return (
+    <header className={styles["header"]}>
+      {withBackButton && (
+        <button
+          className={styles["back-button"]}
+          title="back"
+          onClick={() => redirectBackOr()}
+        >
+          <FaArrowLeft />
+        </button>
+      )}
+      {title}
+    </header>
+  );
 }
