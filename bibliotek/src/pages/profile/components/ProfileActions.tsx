2dec452065136f102c0ac99ebb3a40d735c44bdd
MariusG21 <gheoneamarius10@gmail.com>
2025-08-15 22:03:34 +0300

    small ui detail

--- bibliotek/src/pages/profile/components/ProfileActions.tsx
+++ bibliotek/src/pages/profile/components/ProfileActions.tsx
@@ -1,14 +1,16 @@
+import { useScreenSizeContext } from "@/context/screenSize/useScreenSizeContext";
 import { OutlineButton } from "@/components/Buttons/OutlineButton";
 import { ChangePasswordButton } from "@/components/Buttons/ChangePasswordButton";
 import { DeleteAccountButton } from "@/components/Buttons/DeleteAccountButton";
 import styles from "./ProfileActions.module.css";
 
 export function ProfileActions() {
+  const { isSmall } = useScreenSizeContext();
   return (
     <div className={styles["profile-actions"]}>
-      <OutlineButton action="logout" label="Logout" width={16} />
-      <ChangePasswordButton />
-      <DeleteAccountButton />
+      <OutlineButton action="logout" label="Logout" width={isSmall ? 14 : 16} />
+      <ChangePasswordButton width={isSmall ? 14 : 16} />
+      <DeleteAccountButton width={isSmall ? 14 : 16} />
     </div>
   );
 }
