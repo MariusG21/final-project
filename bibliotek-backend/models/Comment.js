ea9e196eef6b74401f160397d2ee221213b18c51
MariusG21 <gheoneamarius10@gmail.com>
2025-08-16 20:06:17 +0300

    define Comment Model

---
+++ bibliotek-backend/models/Comment.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "./sequelize.js";
+
+export const Comment = sequelize.define("comment", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  bookId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  edited: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
