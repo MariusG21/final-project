4324e246c95cfd639c5d612496ff573e65551aa7
MariusG21 <gheoneamarius10@gmail.com>
2025-08-17 18:47:18 +0300

    remove updateAt property from Comment model

--- bibliotek-backend/models/Comment.js
+++ bibliotek-backend/models/Comment.js
@@ -1,26 +1,33 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "./sequelize.js";
 
-export const Comment = sequelize.define("comment", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
+export const Comment = sequelize.define(
+  "comment",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    bookId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    edited: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  userId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  bookId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  edited: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-});
+  {
+    timestamps: true,
+    updatedAt: false,
+  }
+);
