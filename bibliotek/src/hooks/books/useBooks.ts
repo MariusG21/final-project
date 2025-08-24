d80aa4561f31f6704b1ba318f2ae8474efcf0779
MariusG21 <gheoneamarius10@gmail.com>
2025-08-24 20:10:12 +0300

    fetch pagination details along with books and crete pagination actions

--- bibliotek/src/hooks/books/useBooks.ts
+++ bibliotek/src/hooks/books/useBooks.ts
@@ -8,17 +8,33 @@ export function useBooks() {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const [searchParams] = useSearchParams();
+  const [pagination, setPagination] = useState<{
+    totalBooks: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  } | null>(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get("search");
+  const page = searchParams.get("page") ?? "1";
+  const limit = searchParams.get("limit") ?? "5";
 
   const fetchBooks = useCallback(
     async (retry = false) => {
-      const url = search ? `/api/books?search=${search}` : "/api/books";
+      const url = search
+        ? `/api/books?search=${search}`
+        : `/api/books?page=${page}&limit=${limit}`;
       setIsLoading(true);
       setError(null);
       try {
         const { data } = await axios.get(url);
-        setBooks(data.data);
+        if (data.success) {
+          setBooks(data.data);
+          if (data.pagination) {
+            setPagination(data.pagination);
+          }
+        }
       } catch (error) {
         if (!retry) return fetchBooks(true);
 
@@ -38,12 +54,37 @@ export function useBooks() {
         setIsLoading(false);
       }
     },
-    [search]
+    [search, page, limit]
   );
 
   useEffect(() => {
     fetchBooks();
   }, [fetchBooks]);
 
-  return { books, error, isLoading };
+  const goToNextPage = () => {
+    const nextPage = (Number(page) || 1) + 1;
+    setSearchParams({ page: String(nextPage), limit });
+  };
+
+  const goToPrevPage = () => {
+    if (Number(page) === 1) return;
+    const previousPage = (Number(page) || 1) - 1;
+    setSearchParams({ page: String(previousPage), limit });
+  };
+
+  const goToPage = (pageNumber: number) => {
+    if (pageNumber < 1) return;
+    setSearchParams({ page: String(pageNumber), limit });
+  };
+
+  const paginationActions = { goToNextPage, goToPrevPage, goToPage };
+
+  return {
+    books,
+    error,
+    isLoading,
+    paginationActions,
+    pagination,
+    search,
+  };
 }
